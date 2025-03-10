0e912816b933aba0cd2f70bbaf6925376d79d766
samro <sam.rotthier@gmail.com>
2025-03-10 20:30:47 +0100

    Adding functionality to check the chain validity

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -10,7 +10,7 @@ function Blockchain(){
     this.networkNodes = [];
 
     this.createNewBlock(100,'0', '0'); //this is the genesis block
-}
+};
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
     const newBlock = {
@@ -26,11 +26,11 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
     this.chain.push(newBlock);
 
     return newBlock
-}
+};
 
 Blockchain.prototype.getLastBlock = function(){
     return this.chain[this.chain.length -1];
-}
+};
 
 Blockchain.prototype.createNewTransaction= function(amount, sender, recipient){
     const newTransaction = {
@@ -41,18 +41,18 @@ Blockchain.prototype.createNewTransaction= function(amount, sender, recipient){
     };
 
     return newTransaction;
-}
+};
 
 Blockchain.prototype.addTransactionToPendingTransations = function(transactionObj){
     this.pendingTransactions.push(transactionObj);
     return this.getLastBlock()['index'] +1;
-}
+};
 
 Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce){
     const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData)
     const hash = sha256(dataAsString);
     return hash;
-}
+};
 
 Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData){
     let nonce = 0;
@@ -66,7 +66,30 @@ Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData)
     //console.log(hash)
     return nonce;
 
-}
+};
+
+Blockchain.prototype.chainIsValid = function(blockchain){
+    let validChain = true;
+
+    for (var i = 1; i < blockchain.length; i++){ //we start at position one to skip the genesis block
+        const currentBlock = blockchain[i];
+        const prevBlock = blockchain[i - 1];
+        const blockHash = this.hashBlock(prevBlock['hash'], { transactions: currentBlock['transactions'], index: currentBlock['index'] }, currentBlock['nonce']);
+        if (blockHash.substring(0,4 !== '0000')) validChain = false;
+        if (currentBlock['previousBlockHash'] !== prevBlock['hash']) validChain = false;
+    };
+
+    const genesisBlock = blockchain[0];
+    const correctNonce = genesisBlock['nonce'] === 100;
+    const correctPreviousBlockHash = genesisBlock['previousBlockHash'] === '0';
+    const correctHash = genesisBlock['hash'] === '0';
+    const correctTransactions = genesisBlock['transactions'].length === 0;
+
+    if (!correctNonce || !correctPreviousBlockHash || !correctHash || !correctTransactions) validChain = false;
+
+
+    return validChain;
+};
 
 
 
